229534c0337308d9c11c56db43134a05f075e3f2
Nazlı Aksoy <nazliaksoy@outlook.com>
2017-03-22 18:07:36 +0300

    Add project files.

---
+++ WEHEMO.Business/ICustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WEHEMO.Business
+{
+    public interface ICustomer
+    {
+        Guid Add(string name, string email, string password);
+
+        void Update(Guid cutomerId, string name, string email, string password);
+
+        Guid? Login(string email, string password);
+
+        void Delete(Guid customerId);
+    }
+}
